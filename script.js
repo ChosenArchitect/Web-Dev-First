c819ef330f2fdf94b35ef8453f3fca9df4e6114f
ChosenArchitect <livetogameon@gmail.com>
2020-11-03 01:04:49 -0600

    Added New Projects

--- script.js
+++ script.js
@@ -22,3 +22,44 @@ function getAge() {
     document.getElementById("myAge").innerHTML = "Are you from the future?";
   }
 }
+
+function changeColorCyan() {
+  var element = document.getElementById("cyan"),
+    style = window.getComputedStyle(element),
+    color = style.getPropertyValue("background-color");
+  if ((color = "cyan")) {
+    document.getElementById("cyan").style.backgroundColor = "red";
+  } else {
+    document.getElementById("cyan").style.backgroundColor = "cyan";
+  }
+}
+function changeColorYellow() {
+  var element = document.getElementById("yellow"),
+    style = window.getComputedStyle(element),
+    color = style.getPropertyValue("background-color");
+  if ((color = "yellow")) {
+    document.getElementById("yellow").style.backgroundColor = "blue";
+  } else {
+    document.getElementById("yellow").style.backgroundColor = "yellow";
+  }
+}
+function changeColorMagenta() {
+  var element = document.getElementById("magenta"),
+    style = window.getComputedStyle(element),
+    color = style.getPropertyValue("background-color");
+  if ((color = "magenta")) {
+    document.getElementById("magenta").style.backgroundColor = "green";
+  } else {
+    document.getElementById("magenta").style.backgroundColor = "magenta";
+  }
+}
+function changeColorBlack() {
+  var element = document.getElementById("black"),
+    style = window.getComputedStyle(element),
+    color = style.getPropertyValue("background-color");
+  if ((color = "black")) {
+    document.getElementById("black").style.backgroundColor = "white";
+  } else {
+    document.getElementById("black").style.backgroundColor = "black";
+  }
+}
