a5fa175e5bbcf55d0d540c615e6e660a4dde18f3
ChosenArchitect <livetogameon@gmail.com>
2020-11-02 21:14:30 -0600

    Fixed negative number input

--- script.js
+++ script.js
@@ -11,10 +11,14 @@ function getAge() {
   const currentYear = new Date().getFullYear();
   const birthYear = document.getElementById("yearText").value;
   if (birthYear <= currentYear) {
-    const are = 'You Are: ';
-    const age = currentYear - birthYear;
-    document.getElementById("myAge").innerHTML = (are + age);
+    if (birthYear >= 1900) {
+      const are = "You Are: ";
+      const age = currentYear - birthYear;
+      document.getElementById("myAge").innerHTML = are + age;
+    } else {
+      document.getElementById("myAge").innerHTML = "Are you a time traveler?";
+    }
   } else {
-    document.getElementById("myAge").innerHTML = ("Are you from the future?");
+    document.getElementById("myAge").innerHTML = "Are you from the future?";
   }
 }
